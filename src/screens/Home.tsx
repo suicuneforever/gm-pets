aac64e54648892c0d8b0c5577f124d70704db705
Dani Jaramillo <danijaramillo@Danis-MacBook-Pro.local>
2023-02-28 11:48:24 -0500

    added queries in front end

---
+++ src/screens/Home.tsx
@@ -0,0 +1,51 @@
+import { Pet } from '@prisma/client';
+import axios, { AxiosResponse } from 'axios';
+import React from 'react';
+import { useQuery, useQueryClient } from 'react-query';
+
+// query key - array?
+// loading vs fetching
+// invalidating queries
+// setting query state
+// optimistic updates
+
+function Home() {
+  const queryClient = useQueryClient();
+
+  const petsQuery = useQuery<Pet[]>({
+    queryKey: 'pets',
+    queryFn: () => axios.get('http://localhost:3000/pets').then((res) => res.data),
+  });
+
+  const randomPetQuery = useQuery<Pet>('randomPet', () =>
+    axios.get('http://localhost:3000/pets/random').then((res) => res.data),
+  );
+
+  console.log(petsQuery.data);
+
+  if (petsQuery.isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  //   if (isError) {
+  //     return <span>Error: {error.message}</span>;
+  //   }
+
+  // also status === 'success', but "else" logic works, too
+  return (
+    <>
+      {petsQuery.isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        <ul>
+          {petsQuery.data?.map((pet) => (
+            <li key={pet.id}>{pet.name}</li>
+          ))}
+        </ul>
+      )}
+      <span>Random pet: {randomPetQuery.data?.name}</span>
+    </>
+  );
+}
+
+export default Home;
